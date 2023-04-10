4cf6d8b540c7743a0414441eea386aac994d5c64
farooqui ziauddin <f.ziyauddin@gmail.com>
2023-04-10 21:37:58 +0530

    day 2 work

---
+++ conditional-state/pr4.js
@@ -0,0 +1,12 @@
+let   data_user = ("ziya.in");
+let   data_pass = ("ziya1234");
+
+
+let   user_name  = ("ziya.in");
+let   user_pass  = ("ziya1234");
+
+if(data_user==user_name && data_pass==user_pass){
+    console.log("Login Successfull")
+  }else{
+    console.log("Failed")
+  }
